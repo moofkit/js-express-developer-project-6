e45daca73bcaa23d8bd73ac9760670dcb09781e7
Alexander Shlepin <alex.shlepin64@gmail.com>
2022-11-27 19:23:52 +0300

    init

---
+++ server/locales/ru.js
@@ -0,0 +1,55 @@
+// @ts-check
+
+export default {
+  translation: {
+    appName: 'Task Manager',
+    flash: {
+      session: {
+        create: {
+          success: 'Вы залогинены',
+          error: 'Неправильный емейл или пароль',
+        },
+        delete: {
+          success: 'Вы разлогинены',
+        },
+      },
+      users: {
+        create: {
+          error: 'Не удалось зарегистрировать',
+          success: 'Пользователь успешно зарегистрирован',
+        },
+      },
+      authError: 'Доступ запрещён! Пожалуйста, авторизируйтесь.',
+    },
+    layouts: {
+      application: {
+        users: 'Пользователи',
+        signIn: 'Вход',
+        signUp: 'Регистрация',
+        signOut: 'Выход',
+      },
+    },
+    views: {
+      session: {
+        new: {
+          signIn: 'Вход',
+          submit: 'Войти',
+        },
+      },
+      users: {
+        id: 'ID',
+        email: 'Email',
+        createdAt: 'Дата создания',
+        new: {
+          submit: 'Сохранить',
+          signUp: 'Регистрация',
+        },
+      },
+      welcome: {
+        index: {
+          hello: 'Привет от Wedwyn',
+        },
+      },
+    },
+  },
+};
