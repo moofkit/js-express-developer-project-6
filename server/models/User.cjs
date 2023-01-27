877383f158cb80b716cf07f6ce684bb323dbf652
Alexander Shlepin <alex.shlepin64@gmail.com>
2023-01-27 19:56:51 +0300

    change fields

--- server/models/User.cjs
+++ server/models/User.cjs
@@ -7,16 +7,18 @@ const encrypt = require('../lib/secure.cjs');
 const unique = objectionUnique({ fields: ['email'] });
 
 module.exports = class User extends unique(BaseModel) {
-  static get tableName() {
+  static get tableName() { // указывает, как называется таблица в подключенной базе данных
     return 'users';
   }
 
   static get jsonSchema() {
     return {
       type: 'object',
-      required: ['email', 'password'],
+      required: ['firstname', 'lastname', 'email', 'password'],
       properties: {
         id: { type: 'integer' },
+        firstname: {type: 'string', minLength: 1},
+        lastname: {type: 'string', minLength: 1},
         email: { type: 'string', minLength: 1 },
         password: { type: 'string', minLength: 3 },
       },
@@ -28,6 +30,8 @@ module.exports = class User extends unique(BaseModel) {
   }
 
   verifyPassword(password) {
+    console.log(encrypt(password));
+    console.log(this.passwordDigest);
     return encrypt(password) === this.passwordDigest;
   }
 }
